bf45c8cbfae7358cce16d2f6dd736504c992f74e
github username <github email address>
2019-04-05 09:41:15 -0400

    login and search working

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,4 +5,15 @@ class Api::V1::UsersController < ApplicationController
     render json: @users
   end
 
+  def create
+    @user = User.create(user_params)
+    render json: @user
+  end
+
+private
+
+def user_params
+  params.permit(:name, :username, :password, :email, :phone)
+end
+
 end
--- app/controllers/api/v1/yelp_controller.rb
+++ app/controllers/api/v1/yelp_controller.rb
@@ -3,74 +3,17 @@ require "optparse"
 
 class Api::V1::YelpController < ApplicationController
 
-  # def index
-  #   @restaurants = Restaurant.all
-  #   render json: @restaurants
-  # end
-
-  def fetch
-  rest_name = params[:_json]
-  response = RestClient::Request.execute(
-    method: :get,
-    url: "https://api.yelp.com/v3/businesses/search?term=#{rest_name}&location=charlotte",
-    headers: {Authorization: "Bearer 8ESWJF5yfJ2HIjpxhoXJVnZ9CuXnQHtLMIlSgVwsGu1-Sr5VWaDhSThfTM6riZ-HbnTY_xjp8vbPk7wKEgwlSrpmTMFjzrwV9jy1X0CjQzzd-8AYhAbqfTcYasmgXHYx"}
-  )
-    results = JSON.parse(response)
-    render json: results
-end
 
 def search
-rest_name = params[:_json]
+rest_cuisine = params[:cuisine]
+rest_location = params[:location]
 response = RestClient::Request.execute(
   method: "GET",
-  url: "https://api.yelp.com/v3/businesses/search?location=#{rest_name}",
+  url: "https://api.yelp.com/v3/businesses/search?term=#{rest_cuisine}&location=#{rest_location}&categories=restaurants",
   headers: {Authorization: "Bearer 8ESWJF5yfJ2HIjpxhoXJVnZ9CuXnQHtLMIlSgVwsGu1-Sr5VWaDhSThfTM6riZ-HbnTY_xjp8vbPk7wKEgwlSrpmTMFjzrwV9jy1X0CjQzzd-8AYhAbqfTcYasmgXHYx"}
 )
   results = JSON.parse(response)
   render json: results
 end
-
-# def search
-#   zipcode = params[:zipcode]
-#   @json = Restaurant.find_or_build(zipcode)
-#   render json: @json
-# end
-
-# def create(searchTerm)
-#   rest_name = params[:_json]
-#   response = RestClient::Request.execute(
-#   method: :post,
-#   url: "https://api.yelp.com/v3/businesses/search?term=#{rest_name}&location=#{searchTerm}",
-#   headers: {Authorization: "Bearer 8ESWJF5yfJ2HIjpxhoXJVnZ9CuXnQHtLMIlSgVwsGu1-Sr5VWaDhSThfTM6riZ-HbnTY_xjp8vbPk7wKEgwlSrpmTMFjzrwV9jy1X0CjQzzd-8AYhAbqfTcYasmgXHYx"}
-# )
-#   results = JSON.parse(response)
-#   render json: results
-# end
-
-  # API_HOST = "https://api.yelp.com"
-  # SEARCH_PATH = "/v3/businesses/search"
-  # BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
-  # API_KEY = ENV["YELP_API"]
-  #
-  # DEFAULT_BUSINESS_ID = "yelp-san-francisco"
-  # DEFAULT_TERM = "dinner"
-  # DEFAULT_LOCATION = "New York, NY"
-  # SEARCH_LIMIT = 5
-  #
-  # def self.search(term, location="new york")
-  #   url = "#{API_HOST}#{SEARCH_PATH}"
-  #   params = {
-  #     term: term,
-  #     location: location,
-  #     limit: SEARCH_LIMIT
-  #   }
-  #   response = HTTP.auth("Bearer #{API_KEY}").get(url, params: params)
-  #   response.parse["businesses"]
-  #   byebug
-  # end
-  #
-  # byebug
-  #
-  # puts "hello"
 
 end
