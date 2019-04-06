edf67891353d9457cbd3a52f9ff75f90256edcdf
github username <github email address>
2019-04-06 16:53:12 -0400

    add-coupon-and-render

--- app/controllers/api/v1/offers_controller.rb
+++ app/controllers/api/v1/offers_controller.rb
@@ -5,4 +5,16 @@ class Api::V1::OffersController < ApplicationController
     render json: @offers
   end
 
+  def create
+    @offer = Offer.create(offer_params)
+    render json: @offer
+  end
+
+  private
+
+  def offer_params
+    params.permit(:restaurant_id, :offer, :earn_month, :redeem_month)
+  end
+
+
 end
